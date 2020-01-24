bc7a5430352c44ff843832894c4c76b18910493a
J. Fernando González <jfgonzalezm@gmail.com>
2020-01-24 13:16:00 +0100

    first draft of chatbot

--- webApp/app.py
+++ webApp/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
-
+from flask import Flask, render_template, request, jsonify
+from flask_api import status
 app = Flask(__name__,static_folder='static', template_folder='static')
 
+
+### Frontend
 @app.route('/')
 def index():
     return render_template(('main.html'))
@@ -14,6 +16,25 @@ def ds():
 @app.route('/cv')
 def cv():
     return render_template(('/templates/cv.html'))
-    
+
+### Backend
+@app.route('/api/grade/nlp', methods= ['POST'])
+def grade_nlp():
+    score = 0
+    try:
+        payload = request.get_json()
+        action = payload.get('action', None)
+        if 'action' == 'grade':
+            ### call nlp grading function here
+            score = 98
+            pass
+
+        response = {'status': status.HTTP_200_OK, 'score': score, 'action_performed': action}
+        return jsonify(response)
+
+    except Exception as e:
+        response = {'status': status.HTTP_500_INTERNAL_SERVER_ERROR, 'message': '({}) - Something went Wrong! Try Again!'.format(str(e))}
+        return jsonify(response)
+
 if __name__ == '__main__':
     app.run()
--- webApp/static/js/main.js
+++ webApp/static/js/main.js
@@ -1,13 +1,35 @@
 (function () {
     'use strict';
-  
-    angular.module('FinalProjectApp', [])
-  
-    .controller('mainController', ['$scope', '$log',
+    var app = angular.module('FinalProjectApp', ['angular-simple-chat']);
+
+    app.controller('mainController', ['$scope', '$log',
       function($scope, $log) {
+        $scope.text = "hh";
         $scope.getResults = function() {
           $log.log("test");
         };
+      }
+    ]);
+
+    app.controller('amaCtrl', ['$scope', '$log',
+      function($scope, $log) {
+        var vm = this;
+
+        vm.localUser = {userId: 'student', avatar: '', userName: 'Student'};
+        vm.amaUser = {userId: 'AMA', avatar: '', userName: 'AMA Bot'};
+
+
+        vm.sendMessage = function(message) {
+          console.log('Sending Request to BotAPI');
+      };
+
+
+        vm.messages = [{id: '1', text: 'Welcome!', userid: 'AMA', date: '1455120273886'}]
+
+        $scope.$on('simple-chat-message-posted', function() {
+          console.log('onMessagePosted');
+        });
+
       }
     ]);
   
