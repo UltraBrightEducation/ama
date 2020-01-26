5bcde31300b0061b2486653db508c9ec18968d4d
J. Fernando González <jfgonzalezm@gmail.com>
2020-01-26 21:03:09 +0100

    AudioCapture on Front and UI

--- webApp/static/js/main.js
+++ webApp/static/js/main.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    var app = angular.module('FinalProjectApp', ['angular-simple-chat']);
+    var app = angular.module('FinalProjectApp', ['angular-simple-chat','angularAudioRecorder']);
 
     app.controller('mainController', ['$scope', '$log',
       function($scope, $log) {
@@ -11,10 +11,48 @@
       }
     ]);
 
+    app.controller('audioController', ['$rootScope', '$scope', '$timeout', '$http', function ($rootScope, $scope, $timeout, $http) {
+      var vm = this;
+
+      $scope.audio = {};
+      console.log("Loaded", vm.audio);
+      $scope.timeLimit = 10;
+      vm.sendAudioForSTT = function(audiofile) {
+        if (!audiofile) return;
+
+        $rootScope.$broadcast('audioCaptured', {});
+
+        var config = { headers: { 'Content-Type': undefined } };
+
+        $http.post('/api/stt', audiofile, config)
+          .then(function (response) {
+            var data = response.data;
+            var status = response.status;
+            var statusText = response.statusText;
+            var headers = response.headers;
+            var config = response.config;
+
+            console.log("Success", status);
+            return response; 
+        }).catch(function (errorResponse) {
+            console.log("Error", errorResponse.status);
+            throw errorResponse;
+        });
+      }
+
+      $scope.$watch('audio', function(newVal, oldVal) {
+        if (newVal instanceof Blob) vm.sendAudioForSTT(newVal);
+      });
+
+    }]).config(function (recorderServiceProvider) {
+    recorderServiceProvider
+      .forceSwf(false)
+      .withMp3Conversion(false);
+    });
+
     app.controller('amaCtrl', ['$scope', '$log', '$http', 
       function($scope, $log, $http) {
         var vm = this;
-
 
         vm.localUser = {userId: 'student', avatar: '/static/assets/Man_Avatar.gif', userName: 'Student'};
         vm.amaUser = {userId: 'AMA', avatar: '/static/assets/robot_avatar.png', userName: 'AMA Bot'};
@@ -22,7 +60,7 @@
         vm.sendMessage = function(message) {
           console.log('Sending Request to BotAPI:', message.text);
           vm.callAMA(message.text);
-      };
+        };
 
         vm.messages = [{id: '1', avatar: vm.amaUser.avatar, text: "Welcome! I'm the AMA Bot!", userid: 'AMA', date: Math.floor(Date.now())}]
 
@@ -48,6 +86,10 @@
           message = {id: new Date().valueOf(), userid: vm.amaUser.userId, avatar: vm.amaUser.avatar, text: message, date: Math.floor(Date.now())};
           vm.messages.push(message);
         }
+
+        $scope.$on('audioCaptured', function() {
+          vm.messages.push({id: new Date().valueOf(), avatar: vm.amaUser.avatar, text: "Analyzing your voice input...", userid: 'AMA', date: Math.floor(Date.now())})
+        })
       }
     ]);
   }());
